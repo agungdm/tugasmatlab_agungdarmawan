5a5d2353cf672aa12385afe827df5c706804bb3b
agungdm <147783299+agungdm@users.noreply.github.com>
2023-10-31 10:26:12 +0700

    Add files via upload

---
+++ AGUNGDARMAWAN112020060_TUGAS42.m
@@ -0,0 +1,16 @@
+%Muhammad Agung Darmawan
+%112020060
+%PEMOGRAMAN KOMPUTER D
+
+clear
+clc
+%matrik 5x5
+A = eye(5,5)
+%matrik 3x3 dengan elemen nol
+B = zeros(3,3)
+%matrik 4x4 elemen 1
+C = ones(4,4)
+% matrik 3x4 elemen terdistribusi antara 0 dan 1
+D = rand(3,4)
+%matrix 3x4  elemen terdistribusi normal
+E = randn(3,4)
